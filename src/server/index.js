7f827762a2741f4cdcad6222ae9de54890f00520
Pavel Ravits <vim55k@gmail.com>
2018-09-13 20:22:33 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -9,4 +9,4 @@ app.use(express.static('dist'))
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
 // app.use('*', (req, resp) => resp.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html')))
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log('Listening on port 8080!'))
+app.listen(PORT, () => console.log('Listening on port', PORT))
