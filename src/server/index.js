e1523043ec5963d3a291e4b0fce436259ff718eb
Pavel Ravits <vim55k@gmail.com>
2018-09-14 02:10:14 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
-const expressStaticGzip = require("express-static-gzip")
+const expressStaticGzip = require('express-static-gzip')
 const path = require('path')
 
 const os = require('os')
 
 const app = express()
 
-app.use(expressStaticGzip("dist"))
+const dist = path.resolve(__dirname, '../../dist')
+app.use(expressStaticGzip(dist))
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
-app.use('*', (req, resp) => resp.sendFile(path.resolve(__dirname, '../../dist/index.html')))
+app.use('*', (req, resp) => resp.sendFile(path.resolve(dist, 'index.html')))
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening on port', PORT))
