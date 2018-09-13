dc662497f9a7b14dde2c3b0d161396d41c3ee238
Pavel Ravits <vim55k@gmail.com>
2018-09-13 20:32:19 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,6 @@ const app = express()
 
 app.use(express.static('dist'))
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
-app.use('*', (req, resp) => resp.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html')))
+app.use('*', (req, resp) => resp.sendFile(path.resolve(__dirname, '../../dist/index.html')))
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening on port', PORT))
