dffe98db4a7374308701ce3d790a88884c09661e
Pavel Ravits <vim55k@gmail.com>
2018-09-14 02:18:02 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,14 @@ const os = require('os')
 const app = express()
 
 const dist = path.resolve(__dirname, '../../dist')
-app.use(expressStaticGzip(dist))
+app.use(express.static(dist))
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }))
+app.get('*.js', function(req, res, next) {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/javascript');
+    next();
+  });
 app.use('*', (req, resp) => resp.sendFile(path.resolve(dist, 'index.html')))
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening on port', PORT))
