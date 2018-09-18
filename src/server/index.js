00c8964030dfb1c520c598d41bc4d60cd2d74dbe
Pavel Ravits <vim55k@gmail.com>
2018-09-18 23:29:01 +0300

    server auth

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,37 @@
 import massive from 'massive'
 import express from 'express'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 import * as users from './data/users'
+import { validateTokenMid, signIn, signUp } from './lib/auth'
+
 require('dotenv').config()
 
 const app = express()
 app.use(express.static('dist'))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/api/sign-in', signIn)
+app.post('/api/sign-up', signUp)
+app.use('/api/*', validateTokenMid)
+
+app.get('/api/check', (req, res) =>
+	res.json({ sucess: true, check: true, secret: process.env.JWT_SECRET })
+)
+
+app.use(function(err, req, res, next) {
+	res.status(500).json({ sucess: false, err })
+})
+
+app.use((req, res) => res.sendFile(path.resolve(__dirname, '../../dist/index.html')))
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log('Listening on port', PORT))
+
 app.get('/api/create', (req, res) => {
-	console.log('creating')
 	users
 		.create()
 		.then(() => res.json({ success: true }))
@@ -21,24 +43,15 @@ app.get('/api/drop', (req, res) => {
 		.drop()
 		.then(() => res.json({ success: true }))
 		.catch((err) => res.json({ error: err }))
-	console.log('droping')
 })
 
 app.get('/api/insert', (req, res) => {
-	console.log('req', req.headers)
-	console.log('req', req.rawHeaders)
-	console.log('inserting')
-	app.get('db')
-		.users.insert({ name: 1, email: 2 * Math.random() * 10, password: 3 })
+	const db = req.app.get('db')
+	db.users
+		.insert({ name: 1, email: 2 * Math.random() * 10, password: 3 })
 		.then((user) => res.json({ user, success: true }))
 		.catch((err) => res.json({ error: err }))
 })
-
-app.use('*', (req, resp) =>
-	resp.sendFile(path.resolve(__dirname, '../../dist/index.html'))
-)
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log('Listening on port', PORT))
 
 massive({
 	host: 'localhost',
--- src/server/lib/auth.js
+++ src/server/lib/auth.js
@@ -3,7 +3,96 @@ import bcrypt from 'bcrypt'
 
 function tokenForUser(user) {
 	const timestamp = new Date().getTime()
-	encode({ sub: user.id, iat: timestamp }, process.env.JWT_SECRET)
+	return encode({ sub: user.id, iat: timestamp }, process.env.JWT_SECRET)
 }
 
-function signUp
+function userIdForToken(token) {
+	decode(token, process.env.JWT_SECRET)
+}
+
+export function signUp(req, res, next) {
+	const { app, body } = req
+	const { name, email, password } = body
+	const db = app.get('db')
+	const saltRounds = 12
+
+	if (!email || !password) {
+		res.status(500).json({ error: 'Provide password or email' })
+	}
+	bcrypt
+		.hash(password, saltRounds)
+		.then((hash) => {
+			return db.users
+				.insert({ name, email, password: hash })
+				.then((user) => {
+					res.status(200).json({ sucess: true, token: tokenForUser(user) })
+				})
+				.catch((err) =>
+					res
+						.status(500)
+						.json({ sucess: false, err, error: 'error saving user' })
+				)
+		})
+		.catch((err) => {
+			next(err)
+		})
+}
+
+export function signIn(req, res, next) {
+	const { app, body } = req
+	const { email, password } = body
+	const db = app.get('db')
+
+	if (!email || !password) {
+		res.status(500).json({ sucess: false, error: 'Provide password or email' })
+	}
+
+	return db.users
+		.findOne({ email })
+		.then((validUser) => {
+			bcrypt
+				.compare(password, validUser.password)
+				.then((validPassword) => {
+					if (validPassword) {
+						res.status(200).json({
+							sucess: true,
+							token: tokenForUser(validUser),
+						})
+					}
+				})
+				.catch((err) =>
+					res
+						.status(401)
+						.json({ sucess: false, err, error: 'Unauthorized Access' })
+				)
+		})
+		.catch((err) =>
+			res.status(500).json({ sucess: false, err, error: 'user not found' })
+		)
+}
+
+export function validateTokenMid(req, res, next) {
+	const { app, body, headers } = req
+	console.log('req', req)
+	const { token } = headers
+	const db = app.get('db')
+
+	if (!token) {
+		res.status(500).json({ sucess: false, err: 'no token, not authorized' })
+	}
+
+	const userId = userIdForToken(token)
+
+	return db.users
+		.findOne({ id: userId })
+		.then((validUser) => {
+			req.user = validUser
+			console.log('validUser', validUser)
+			next()
+		})
+		.catch((err) =>
+			res
+				.status(500)
+				.json({ sucess: false, err, error: 'no user, not authorized' })
+		)
+}
